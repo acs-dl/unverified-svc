3c1991d22df69ca5dbbad09a9d993b401aaf8861
Maksym Hrynenko <mg06062003@gmail.com>
2023-02-24 14:53:54 +0200

    indexes

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -13,10 +13,18 @@ CREATE TABLE IF NOT EXISTS users (
     UNIQUE (module_id, module)
 );
 
-CREATE INDEX IF NOT EXISTS users_idx ON users(username, phone, email, module, module_id);
+CREATE INDEX IF NOT EXISTS users_username_idx ON users(username);
+CREATE INDEX IF NOT EXISTS users_phone_idx ON users(phone);
+CREATE INDEX IF NOT EXISTS users_email_idx ON users(email);
+CREATE INDEX IF NOT EXISTS users_module_idx ON users(module);
+CREATE INDEX IF NOT EXISTS users_moduleId_idx ON users(module_id);
 
 
 -- +migrate Down
 
 DROP TABLE IF EXISTS users;
-DROP INDEX IF EXISTS users_idx;
+DROP INDEX IF EXISTS users_username_idx;
+DROP INDEX IF EXISTS users_phone_idx;
+DROP INDEX IF EXISTS users_email_idx;
+DROP INDEX IF EXISTS users_module_idx;
+DROP INDEX IF EXISTS users_moduleId_idx;
