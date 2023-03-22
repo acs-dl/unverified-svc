574cdaa02ecdbcc649b6197d92fde9f2dd5de76f
Maksym Hrynenko <mg06062003@gmail.com>
2023-03-22 16:22:04 +0200

    change import path

--- internal/service/api/handlers/get_user_roles_map.go
+++ internal/service/api/handlers/get_user_roles_map.go
@@ -3,7 +3,7 @@ package handlers
 import (
 	"net/http"
 
-	"gitlab.com/distributed_lab/acs/identity-svc/resources"
+	"gitlab.com/distributed_lab/acs/unverified-svc/resources"
 	"gitlab.com/distributed_lab/ape"
 )
 
